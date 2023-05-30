303cb27ac318ef69b1daccc33b3714a62b44c19d
pavans2011 <135034771+pavans2011@users.noreply.github.com>
2023-05-30 19:33:56 +0530

    Create While_Loop_Bug.py
    This is a very simple while loop which is meant for destroying any python program without any debugger.

---
+++ While_Loop_Bug.py
@@ -0,0 +1,16 @@
+    
+#A very simple bug, literally a three line code, which can crash and instantly kill any program that uses python as it's main code, I, two dots, present to 🫵, the loop paradox bug!
+
+#But there is one minor (*Major) problem, This compiler does not want you to execute this peice of code, because, well the app will crash, and mimo is not intelligent enough to tacle a three line code. (*No offence!)
+
+#So I added a variable and assigned it a value, hence the compiler will execute the code without 💥ing itself, even though it 💥s itself if you plug even 10000 into the variable.
+
+stop_the_bug = 10000
+#My OG plan of plugging a value: 100000000000000000000000000000000000 (*One hundred Decellion.)
+
+while stop_the_bug > 0:
+  print("Good luck fixing this!")
+  stop_the_bug -= 1
+
+
+
